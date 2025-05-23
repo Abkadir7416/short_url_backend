caef5b3b73b7cd832beed34e575123f42cff2aa5
Abdul Kadir (Technology - New Delhi) <abdul.kadir@partner.htmedialabs.com>
2025-05-23 23:13:38 +0530

    remove port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
