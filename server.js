cb2c4b526a808af82da2e5f6f56d12369a4bfa75
Abdul Kadir (Technology - New Delhi) <abdul.kadir@partner.htmedialabs.com>
2025-05-24 11:22:49 +0530

    create url-short and redirect API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import connectDB from './config/mongo.config.js'
 import dotenv from 'dotenv';
+import shortUrlRoutes from './routes/short_url.route.js';
 
 const app = express();
 
@@ -16,6 +17,10 @@ connectDB();
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to URL Shortener API' });
 });
+
+
+// Routes
+app.use('/api', shortUrlRoutes);
 
 // Start server
 const PORT = 5000;
